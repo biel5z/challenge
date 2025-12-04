7492d1be3d538478176d3bb526da11882f45b83f
Gabriel Correia Reis <correiareisg@gmail.com>
2025-12-04 16:29:29 -0300

    Tipos de dados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
-let cidade = "Jandira"
-let estado = "SP"
+let nome = "Paulo"; // String
+let idade = 28; // Number
+let logado = true // True ou False (Boolean)
+let lista = ["ovo", "massa", "corante"] // Array - (lista de algo) que se abre com colchetes []
+let nomeCompleto = { nome: "bobo", idade: 90} // Objeto - (lista de algo) que se abre com chaves {}
 
-console.log(cidade + "/" + estado)
+
+console.log(typeof lista)
