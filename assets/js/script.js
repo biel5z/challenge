fd1cf4dc25e511cc2a289410eb47f6ba061daa5f
Gabriel Correia Reis <correiareisg@gmail.com>
2025-12-15 15:44:42 -0300

    Exercício de condicional #2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
-let idade = 0;
+let n1 = 10;
+let n2 = 5;
+console.log(n1 > n2)
 
-let adulto = idade >= 18 && idade < 60;
+let w = "10";
+let z = 10;
+console.log(w === z)
 
-if(adulto){
-    console.log("É adulto")
+let a = 10;
+let b = 5;
+console.log(a != b)
+
+let preco = 46;
+
+if (preco <= 45){
+    console.log("Está barato")
 } else {
-    console.log("NÃO é adulto")
+    console.log("está caro")
 }
