39bc5017b3e2cebd14c1e8864a87d65a83445328
Gabriel Correia Reis <correiareisg@gmail.com>
2025-12-15 14:26:18 -0300

    else if

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 
+let idade = 59;
 
+if(idade < 18){
+    console.log("Você é uma criança.");
+}else if(idade >= 18 && idade < 60){
+    console.log("Você é um adulto")
+}else if(idade >= 60){
+    console.log("Você é um idoso")
+}
 
 
 
