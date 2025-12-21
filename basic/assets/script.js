164bf78638d3adaab019d351f2577b990c62578d
Gabriel Correia Reis <correiareisg@gmail.com>
2025-12-20 22:24:30 -0300

    fcn

--- basic/assets/script.js
+++ basic/assets/script.js
@@ -1,14 +1,6 @@
-function maiorDeIdade(idade){
-    if(idade >= 18){
-        return true;
-    }else {
-        return false;
-    }
-}
-let idade = 0;
-let verificacao = maiorDeIdade(idade);
+function calcularImovel(metragem, quartos){
+    let m2 = 3000;
 
-console.log(verificacao ? "É maior de idade" : "Não é maior de idade");
-// ou
-let mensagem = verificacao ? "Sim, é maior" : "Não, é maior";
-console.log(mensagem);
+    
+
+}
