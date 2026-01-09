fd6a4018127a7ae0495227ca105a490948dca5bf
Gabriel Correia Reis <correiareisg@gmail.com>
2026-01-09 12:14:25 -0300

    variaveis dentro de funções

--- basic/assets/script.js
+++ basic/assets/script.js
@@ -1,5 +1,8 @@
-// Se a função for utilizada em uma linha só fazer já junto, se não fazer com chaves
-// E se for 1 parametro eu posso remover o parenteses, 
-// const somar = (x, y) => x + y
+let count = 0;
+function add(){
+    count++;
+}
 
+add();
 
+console.log(count)
