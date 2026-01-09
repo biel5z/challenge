12b8c3d64b0ce8e722f33e36f7e19cb4bfbb9fa7
Gabriel Correia Reis <correiareisg@gmail.com>
2026-01-08 23:14:28 -0300

    arrow fuction

--- basic/assets/script.js
+++ basic/assets/script.js
@@ -1,44 +1,5 @@
-function validar(usuario, senha){
-    if(usuario === "Pedro" && senha === '123'){
-        return true;
-    }else {
-        return false;
-    }
-}
-
-let usuario = 'Pedr';
-let senha = '123'
-let validacao = validar(usuario, senha);
-if (validacao){
-    console.log("Acesso concedido.");
-}else{
-    console.log("Acesso NEGADO!")
-}
+// Se a função for utilizada em uma linha só fazer já junto, se não fazer com chaves
+// E se for 1 parametro eu posso remover o parenteses, 
+// const somar = (x, y) => x + y
 
 
-
-// function calcularImovel(metragem, quartos){
-//     let m2 = 3000;
-//     let preco = 0;
-
-//     switch(quartos){
-//         case 1:
-//         default:
-//             preco = metragem * m2;
-//             break;
-            
-//         case 2:
-//             preco = metragem * (m2 * 1.2);
-//             break;
-
-//         case 3:
-//             preco = metragem * (m2 * 1.5);
-//             break
-//     }
-//     return preco;
-// }
-
-// let mensagem = 123;
-// let quartos = 3;
-// let preco = calcularImovel(metragem, quartos);
-// console.log(`A casa custa R$ ${preco}`)
