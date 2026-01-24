fb0a2e30e965a6887f5e152bc29adf6c7fc043f4
Gabriel Correia Reis <correiareisg@gmail.com>
2026-01-24 16:20:31 -0300

    Funções de array

---
+++ basic/assets/function-arrays.js
@@ -0,0 +1,24 @@
+let frutas = ['Uva','abacaxi','manga','banana']
+
+frutas[0] = 'Pêra'
+
+console.log(frutas)
+
+
+// Conta a quantida de itens dentro do Array
+console.log(frutas.length)
+
+// Adiciona mais um item na lista
+frutas.push('melancia')
+
+// deleta o ultimo item da lista
+frutas.pop()
+
+// deleta o primeiro item da lista
+frutas.shift
+
+// ele não modifica o array, mas sim 
+frutas.join(' ')
+
+
+frutas[0] = 'Pêra'
