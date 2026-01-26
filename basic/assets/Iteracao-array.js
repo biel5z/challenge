9019c7b1210078751004fe59cc91549be46af9d9
Gabriel Correia Reis <correiareisg@gmail.com>
2026-01-26 20:53:55 -0300

    interação de array

---
+++ basic/assets/Iteracao-array.js
@@ -0,0 +1,17 @@
+let frutas = ['Maça', 'Uva', 'laranja', 'Banana'];
+
+frutas
+
+// let ok = frutas.some((value) => {
+//     return value.length > 3;
+// });
+
+// if (ok){
+//     console.log('Algum é maior que 3');
+// } else {
+//     console.log('Nenhum item é maior que 3')
+// }
+
+// let bigFrutas = frutas.filter((item) => item.length > 4);
+
+// console.log(bigFrutas);
